8908f89cc8bfca0250403d0840a3320ac5ab23b8
Alfin72 <alfin72@gmail.com>
2020-01-07 22:57:14 +0530

    Update Image.py

--- Image.py
+++ Image.py
@@ -1,135 +1,59 @@
-import pyautogui
-
 import selenium
-
 from time import sleep
-
-
-
 
 
 from selenium import webdriver
 
-
-
 browser = webdriver.Chrome("C:\\Users\\Administrator\\Downloads\\chromedriver.exe")
 
-
+a=[]
 
 class Url():
-
     def run(self):
-
         browser.get("https://www.computerhope.com/")
 
-
-
-
-
-class Help():
-
-    def run(self):
-
-        elem = browser.find_element_by_link_text('Help')
-
-        elem.click()
-
-        sleep(3)
-
-
-
-
-
-class Next():
-
-    def run(self):
-
-        elem = browser.find_element_by_link_text('Page categories')
-
-        elem.click()
-
-        sleep(3)
-
-
-
-
-
 class Back():
-
     def run(self):
-
-            sleep(1)
-
             browser.back()
+            sleep(.5)
+            browser.forward()
+            sleep(.5)
+            t5.run()
 
-
-
-
-
-
+class Allow():
+    def run(self):
+        print("clicked")
+        a.append("b")
+        c = len(a)
+        print(c)
+        elem = browser.find_element_by_link_text('Help')
+        elem.click()
+        sleep(.2)
+        t3.run()
 
 class Image():
-
     def run(self):
-
         try:
-
-            ima = browser.find_element_by_xpath('/html/body/div[2]/header/a/img')
-
+            ima = browser.find_element_by_xpath('/html/body/div[2]/*')
             if ima.is_displayed():
-
-                print ("u dont have the opertunity to learn more")
-
-
-
-
-
-        except Exception as e:
-
-            ima2 = browser.find_element_by_xpath('/html/body/div[2]/header/img')
-
-            if ima2.is_displayed():
-
-                print("it's page1")
-
-
-
-
+                print("displayed")
+                a = input("press space and enter to allow just enter to skip")
+                if a==(" "):
+                    t2.run()
+                else:
+                    t3.run()
+        except Exception as E:
+            print("Refreshinggggg")
+            t3.run()
 
 
 
 t1 = Url()
-
-t2 = Help()
-
+t2 = Allow()
 t3 = Back()
-
-t4 = Next()
 
 t5 = Image()
 
 
-
-
-
 t1.run()
-
-t2.run()
-
-t3.run()
-
-
-
-
-
-A = input("enter1")
-
-if A == ("1"):
-
-    print("true")
-
-    t5.run()
-
-else:
-
-    t4.run()
+t5.run()
